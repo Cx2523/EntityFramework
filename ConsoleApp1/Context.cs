4a701b7ecab1fa719bbd04ce8e509ca23a7ad76d
Chris <Chris@PCONAMAC>
2018-01-25 20:40:51 -0700

    Add project files.

---
+++ ConsoleApp1/Context.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Context : DbContext
+    {
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                context.ComicBooks.Add(new ComicBook()
+                {
+                    SeriesTitle = "Spiderman",
+                    IssueNumber = 1,
+                    Description = "Really Neato",
+                    PublishedOn = DateTime.Today
+                });
+
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks.ToList();
+                
+                foreach(var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.SeriesTitle);
+                }
+                Console.WriteLine("Test");
+                Console.ReadLine();
+            }
+        }
+    }
+}
